9f1fb50023e3b5afbc281aff3a509a90a7b4b666
danlei27 <danleisantos@hotmail.com>
2020-04-21 20:19:53 -0300

    correções no banco

--- models/db.js
+++ models/db.js
@@ -3,7 +3,10 @@ const password = require('./senha')
 const Sequelize = require('sequelize');
 let sequelize;
 if(process.env.NODE_ENV == "production"){
-    sequelize = new Sequelize('postgres://lwnvkpeenakrks:fd88d349b6623b5499499c4c6916f9866bb8ad930747febd1c31b8607c7555e4@ec2-34-225-82-212.compute-1.amazonaws.com:5432/dafvhi3t9qcu5a')
+    sequelize = new Sequelize('dafvhi3t9qcu5a','lwnvkpeenakrks','fd88d349b6623b5499499c4c6916f9866bb8ad930747febd1c31b8607c7555e4',{
+        host:'ec2-34-225-82-212.compute-1.amazonaws.com',
+        dialect:'postgres'
+    })
 }else{
     sequelize = new Sequelize('almoxarifado','postgres', password,{
         host:'localhost',
@@ -12,10 +15,10 @@ if(process.env.NODE_ENV == "production"){
     
 }
 sequelize.authenticate().then(function(){
-    console.log('Servidor conectado com sucesso!')
-}).catch(function(err){
-    console.log('Falha ao se conectar:'+err)
-});
+        console.log('Servidor conectado com sucesso!')
+    }).catch(function(err){
+        console.log('Falha ao se conectar:'+err)
+    });
 
 module.exports = {
     Sequelize: Sequelize,
